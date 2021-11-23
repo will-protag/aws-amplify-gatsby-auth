2a0ddeadf6abb96586c3acfd2b5b1ac447c051ca
will-protag <will.jacobs@protagstudios.com>
2021-11-23 10:09:12 -0600

    Adding android download link

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -4,8 +4,9 @@ import { Link } from 'gatsby'
 const Downloads = () => <div>
   <h1>Downloads</h1>
 	{/* <p>Windows Latest (dev): <Link to="/app/profile">View profile</Link></p> */}
-	<p>Windows latest (dev): <a href="https://s3.console.aws.amazon.com/s3/object/ftx-cyball-builds?region=us-west-2&prefix=dev-builds/Windows/Cyball-Windows-debug.zip">Click here</a></p>
-	{/* <p>Now go build something great and deploy it using the <a href="https://console.amplify.aws">AWS Amplify Console</a></p> */}
+	<h2>Cyball</h2>
+		<li>Windows latest (dev): <a href="s3://ftx-cyball-builds/dev-builds/Windows/Cyball-Windows-debug.zip">Click here</a></li>
+		<li>Android latest (dev): <a href="s3://ftx-cyball-builds/dev-builds/Android/Cyball-Android-debug.zip">Click here</a></li>
 	</div>
 
 export default Downloads
