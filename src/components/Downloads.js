47eb9200852eb0b41ab81f7295e84ced6b54639b
will-protag <will.jacobs@protagstudios.com>
2021-11-23 10:23:02 -0600

    Adding download links

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -5,8 +5,8 @@ const Downloads = () => <div>
   <h1>Downloads</h1>
 	{/* <p>Windows Latest (dev): <Link to="/app/profile">View profile</Link></p> */}
 	<h2>Cyball</h2>
-		<li>Windows latest (dev): <a href="s3://ftx-cyball-builds/dev-builds/Windows/Cyball-Windows-debug.zip">Click here</a></li>
-		<li>Android latest (dev): <a href="s3://ftx-cyball-builds/dev-builds/Android/Cyball-Android-debug.zip">Click here</a></li>
+		<li>Windows latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/public/Cyball-Windows-debug.zip">Click here</a></li>
+		<li>Android latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/public/Cyball-Android-debug.zip">Click here</a></li>
 	</div>
 
 export default Downloads
