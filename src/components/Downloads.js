039e8a40cbdb136f5f3afc51debd999ff538b7f9
will-protag <will.jacobs@protagstudios.com>
2021-11-30 20:10:49 -0600

    Adding links to profile page

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -5,8 +5,8 @@ const Downloads = () => <div>
   <h1>Downloads</h1>
 	{/* <p>Windows Latest (dev): <Link to="/app/profile">View profile</Link></p> */}
 	<h2>Cyball</h2>
-		<li>Windows latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/protected/Cyball-Windows-debug.zip">Click here</a></li>
-		<li>Android latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/protected/Cyball-Android-debug.zip">Click here</a></li>
+		<li>Windows latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/protected/Cyball-Windows-debug.zip" download>Click here</a></li>
+		<li>Android latest (dev): <a href="s3://aws-amplify-ptg-portal-storage-dohyxeahtqqv9project-devv/protected/Cyball-Android-debug.zip" download>Click here</a></li>
 	</div>
 
 export default Downloads
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { Link } from 'gatsby'
 
 const Home = () => <div>
   <h1>Home</h1>
-	<p>You are now logged in! <Link to="/app/profile">View profile</Link></p>
+	<p>You are now logged in!</p>
+	<Link to="/app/profile">View profile</Link><br />
+	<Link to="/app/home">Home</Link><br />
+    <Link to="/app/downloads">Downloads</Link><br />
 	{/* <p>Now go build something great and deploy it using the <a href="https://console.amplify.aws">AWS Amplify Console</a></p> */}
 	</div>
 
